8656a94d34f1a29f1d83a9f55f323bfef9d45127
lucas-pvg <lucaspvg1@gmail.com>
2024-06-14 17:02:28 -0300

    💄 Added the links on the App page for visualization

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 // import { Router, Routes, Route } from 'react-router-dom'
+import { LinkGroup } from './components/links/LinkGroup'
 import './App.css'
 
 function App() {
   return (
     <div className='app-container'>
       <h1>Hello World</h1>
+
+      <LinkGroup
+        mode='light'
+        labels={['Aulas', 'Presença', 'Professores', 'Turmas', 'Métricas']}
+        paths={['/', '/', '/']}
+      />
+
     </div>
 
     // <Router>
