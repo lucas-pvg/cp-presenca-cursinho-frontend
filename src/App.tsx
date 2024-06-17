36ffcb6c23da89a9361a2918ffb6c938d517ffdc
Lucas Garieri <lucaspvg1@gmail.com>
2024-06-17 00:26:19 -0300

    💄 Added Table for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 // import { Router, Routes, Route } from 'react-router-dom'
+import { Table } from './components/table/Table'
+import { Class, classes } from './data/classes'
 import './App.css'
 
 function App() {
+  const classData = classes.map((obj) => {
+    return (Object.keys(obj).map((key) => obj[key as keyof Class]))
+  })
+
   return (
     <div className='app-container'>
       <h1>Hello World</h1>
+
+      <Table
+        mode='light'
+        clickable={true}
+        header={['Aula', 'Horário', 'Turma', 'Column']}
+        data={classData}
+      />
     </div>
 
     // <Router>
