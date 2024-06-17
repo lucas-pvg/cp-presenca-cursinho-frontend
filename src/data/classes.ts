0fd92e677d6bdee79fa74122e7508c0aafc58eb5
Lucas Garieri <lucaspvg1@gmail.com>
2024-06-17 00:26:38 -0300

    🗃️ Added more classes

--- src/data/classes.ts
+++ src/data/classes.ts
@@ -35,5 +35,305 @@ export const classes:Array<Class> = [
     horario: '21h00',
     turma: 'Turma 1',
     column: 'Value'
-  }
+  },
+  {
+    aula: 'Matemática',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Português',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Física',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Química',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Geografia',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Matemática',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Português',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Física',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Química',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Geografia',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Matemática',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Português',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Física',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Química',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Geografia',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Matemática',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Português',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Física',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Química',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Geografia',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Matemática',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Português',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Física',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Química',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Geografia',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Matemática',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Português',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Física',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Química',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Geografia',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Matemática',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Português',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Física',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Química',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Geografia',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Matemática',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Português',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Física',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Química',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Geografia',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Matemática',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Português',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Física',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Química',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Geografia',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Matemática',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Português',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Física',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Química',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
+  {
+    aula: 'Geografia',
+    horario: '21h00',
+    turma: 'Turma 1',
+    column: 'Value'
+  },
 ]
