4d0802a2ddefe9744ebb3ab8a49ebf4390ee82c5
Lucas Garieri <lucaspvg1@gmail.com>
2024-06-21 03:22:47 -0300

    🚀✨ Added Card and Card Menu components

---
+++ src/components/card-menu/card-menu.tsx
@@ -0,0 +1,25 @@
+import { Card } from './card'
+import './card.css'
+
+interface cardMenuProps {
+	className: string
+	mode?: 'light' | 'dark'
+	labels: Array<string>
+	to: Array<string>
+}
+
+export function CardMenu({ className, mode, labels, to }: cardMenuProps) {
+	const classes = `${className} card-menu`
+
+	return (
+		<div className={classes}>
+			{
+				labels.map((label, i) => {
+					return (
+						<Card to={to[i]} mode={mode} label={label} key={i} />
+					)
+				})
+			}
+		</div>
+	)
+}
