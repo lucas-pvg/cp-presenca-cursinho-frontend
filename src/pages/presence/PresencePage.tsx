95f71a5405e50ebc1f4fd9b0f99c81b77c0a830c
VictorASantana <victor.asantana012@gmail.com>
2024-06-27 23:34:48 -0300

    fix: adjusting presence-page

--- src/pages/presence/PresencePage.tsx
+++ src/pages/presence/PresencePage.tsx
@@ -1,4 +1,5 @@
 import { Header } from '../../components/header/header';
+import { Hero } from '../../components/hero/hero';
 import { Table } from '../../components/table/Table';
 import { Student, students } from '../../data/students';
 
@@ -8,12 +9,14 @@ import './PresencePage.css';
 export const PresencePage = () => {
     return (
         <div className='presence-container'>
-            <Header to={''} title={'Controle de Presença'} subtitle='Tenha acesso às informações de uma aula e à lista de presença gerada.'/>
+            <div className='presence-header'>
+                <Header to={''} />
+                <Hero heroTitle={'Consultar Presença'} heroDescription='Tenha acesso às informações de uma aula e à lista de presença gerada.'/>
+            </div>
             <div className='presence-body'>
                 <div className='presence-table'>
                     <Table header={['Nome do aluno', 'Presença']} data={studentsData} />
                 </div>
-                
             </div>
         </div>
     )
