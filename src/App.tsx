d1160d82f1e5849fbceef83a677a15799d5d4347
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-07-10 21:19:31 -0300

    update table component by passing any child component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,50 @@
-import { useState } from 'react'
-import { Routes, Route, useLocation } from 'react-router-dom'
-import { Navbar } from './components/navbar/Navbar'
-import { Header } from './components/header/header'
-import { CreateClass } from './components/modal/create-class'
-import { Button } from './components/button/Button'
-import './App.css'
+import { useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { Navbar } from "./components/navbar/Navbar";
+import { Header } from "./components/header/header";
+import { CreateClass } from "./components/modal/create-class";
+import { Button } from "./components/button/Button";
+import "./App.css";
 
-import { HomePage } from './pages/home-page/HomePage'
+import { HomePage } from "./pages/home-page/HomePage";
+import { PresencePage } from "./pages/presence/PresencePage";
 
 function App() {
-  const location = useLocation()
-  const [ open, setOpen ] = useState(false)
+	const location = useLocation();
+	const [open, setOpen] = useState(false);
 
-  return (
-    <div className='app-container'>
-      <Navbar mode='light' />
+	return (
+		<div className="app-container">
+			<Navbar mode="light" />
 
-      <div className='page-container'>
-        <Header to={location}/>
+			<div className="page-container">
+				<Header to={location} />
 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/attendence" element={<h1 className='center'>Presença</h1>} />
-          <Route path="/teachers" element={<h1 className='center'>Professores</h1>} />
-          <Route path="/classes" element={<h1 className='center'>Turmas</h1>} />
-          <Route path="/metrics" element={
-            <div className='center'>
-              <Button onClick={() => setOpen(true)}>Open Modal</Button>
-              <CreateClass className={open ? 'modal-open' : 'modal-close'} mode='light' close={() => setOpen(false)} />
-            </div>
-          } />
-        </Routes>
-      </div>
-    </div>
-  )
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/attendence" element={<PresencePage />} />
+					<Route
+						path="/teachers"
+						element={<h1 className="center">Professores</h1>}
+					/>
+					<Route path="/classes" element={<h1 className="center">Turmas</h1>} />
+					<Route
+						path="/metrics"
+						element={
+							<div className="center">
+								<Button onClick={() => setOpen(true)}>Open Modal</Button>
+								<CreateClass
+									className={open ? "modal-open" : "modal-close"}
+									mode="light"
+									close={() => setOpen(false)}
+								/>
+							</div>
+						}
+					/>
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
