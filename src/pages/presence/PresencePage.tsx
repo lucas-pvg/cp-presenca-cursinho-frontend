65328a478cef3a7c80796167495cfeabdddb1e25
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-07-10 21:19:59 -0300

    refactor: update presence table according to new pattern

--- src/pages/presence/PresencePage.tsx
+++ src/pages/presence/PresencePage.tsx
@@ -1,43 +1,78 @@
-import { cva, VariantProps } from 'class-variance-authority';
-import { Hero } from '../../components/hero/hero';
-import { Table } from '../../components/table/Table';
-import { Student, students } from '../../data/students';
+import { cva, VariantProps } from "class-variance-authority";
+import { Hero } from "../../components/hero/hero";
+import { Table } from "../../components/table/Table";
+import { Student, studentsMock } from "../../data/students";
 
-import './PresencePage.css';
+import "./PresencePage.css";
+import React from "react";
+import { Button } from "../../components/button/Button";
 
-const PresencePageVariants = cva(
-    'presence page',
-    {
-      variants: {
-        mode: {
-          light: 'light',
-          dark: 'dark'
-        }
-      },
-      defaultVariants: {
-        mode: 'light'
-      }
-    }
-  )
+const PresencePageVariants = cva("presence page", {
+	variants: {
+		mode: {
+			light: "light",
+			dark: "dark",
+		},
+	},
+	defaultVariants: {
+		mode: "light",
+	},
+});
 
-  interface PresencePageProps extends VariantProps<typeof PresencePageVariants> {
-    mode?: 'light' | 'dark'
-  }
-
-export const PresencePage = ({ mode, ...props}: PresencePageProps) => {
-    return (
-        <div className={PresencePageVariants({ mode })} {...props}>
-            <Hero title={'Consultar Presença'} description='Tenha acesso às informações de uma aula e à lista de presença gerada.'/>
-            <div className='page-content'>
-                <Table header={['Nome do aluno', 'Presença']} data={studentsData} />
-            </div>
-        </div>
-    )
+interface PresencePageProps extends VariantProps<typeof PresencePageVariants> {
+	mode?: "light" | "dark";
 }
 
-const studentsData = students.map((obj) => {
-    return (Object.keys(obj).map((key) => obj[key as keyof Student]))
-  })
+export const PresencePage = ({ mode, ...props }: PresencePageProps) => {
+	const [studentsData, setStudentsData] =
+		React.useState<Array<Student>>(studentsMock);
 
+	// useEffect(() => {
+	// 	setStudentsData(students);
+	// });
 
+	const createTableDataFromStudentsData = () => {
+		return studentsData.map((student, index) => [
+			student.name,
+			<Button
+				onClick={(e) => {
+					e.preventDefault();
 
+					const newPresence =
+						student.presence === "ausente" ? "presente" : "ausente";
+
+					setStudentsData((prevStudents) => {
+						const newStudents = [...prevStudents];
+						newStudents[index] = {
+							...newStudents[index],
+							presence: newPresence,
+						};
+						return newStudents;
+					});
+				}}
+				className={
+					student.presence === "presente"
+						? "presence-check-button"
+						: "abscense-button"
+				}
+			>
+				{student.presence}
+			</Button>,
+		]);
+	};
+
+	return (
+		<div className={PresencePageVariants({ mode })} {...props}>
+			<Hero
+				title={"Consultar Presença"}
+				description="Tenha acesso às informações de uma aula e à lista de presença gerada."
+			/>
+			<div className="page-content">
+				<Table
+					header={["Nome do aluno", "Presença"]}
+					data={createTableDataFromStudentsData()}
+				/>
+			</div>
+		</div>
+	);
+};
