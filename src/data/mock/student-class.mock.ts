6d7a650eaac4774635e32c47c96c6bd54e43d9f9
VictorASantana <victor.asantana012@gmail.com>
2024-07-28 16:13:51 -0300

    feat: setting up student class page

---
+++ src/data/mock/student-class.mock.ts
@@ -0,0 +1,10 @@
+export const rows = [
+  [
+    { title: 'Horário: ', subtitle: (new Date()).toDateString() },
+    { title: 'Sala', subtitle: 'T1-02' }
+  ],
+  [
+    { title: 'Curso', subtitle: 'Extensivo' },
+    { title: 'Modalidade', subtitle: 'Presencial' }
+  ]
+];
