0e8401506ed541eeeaf9bc87e1ca32766582ca3c
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-07-27 23:49:52 -0300

    chore: add environment variables

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -1,7 +1,7 @@
 import Axios, { type AxiosResponse } from 'axios';
 
 const axios = Axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: import.meta.env.VITE_API_URL,
   responseType: 'json'
 });
 
