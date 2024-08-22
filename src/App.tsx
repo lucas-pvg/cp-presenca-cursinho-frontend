efc45e443172736ff7df984a8e0d2f0a4567734c
Lucas Garieri <lucaspvg1@gmail.com>
2024-08-22 15:10:05 -0300

    💄 Abstracton of some css classes for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { Navbar } from "./components/navbar/Navbar";
 import { Header } from "./components/header/header";
-import { CreateClass } from "./components/modal/create-class";
-import { Button } from "./components/button/Button";
 import "./App.css";
 
 import { HomePage } from './pages/home-page/HomePage'
@@ -12,7 +9,6 @@ import { LessonDetailPage } from './pages/lesson-detail-page/lesson-detail-page'
 
 function App() {
 	const location = useLocation();
-	const [open, setOpen] = useState(false);
 
 	return (
 		<div className="app-container">
@@ -28,16 +24,8 @@ function App() {
           
           <Route path="/attendence" element={<h1 className='center'>Presença</h1>} />
           <Route path="/teachers" element={<h1 className='center'>Professores</h1>} />
-          <Route path="/classes" element={
-            <LessonDetailPage />
-            // <h1 className='center'>Turmas</h1>
-          } />
-          <Route path="/metrics" element={
-            <div className='center'>
-              <Button onClick={() => setOpen(true)}>Open Modal</Button>
-              <CreateClass className={open ? 'modal-open' : 'modal-close'} mode='light' close={() => setOpen(false)} />
-            </div>
-          } />
+          <Route path="/classes" element={<h1 className='center'>Turmas</h1>} />
+          <Route path="/metrics" element={<h1 className='center'>Métricas</h1>} />
         </Routes>
       </div>
     </div>
