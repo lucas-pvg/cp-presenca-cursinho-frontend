d44ff2a351f853fc9b1243be3013c123c8589d80
Lucas Garieri <lucaspvg1@gmail.com>
2024-09-04 13:26:14 -0300

    Merge branch 'feat/delete-lesson' of https://github.com/lucas-pvg/cp-presenca-cursinho-frontend into feat/delete-lesson

---
+++ src/data/requests/lesson.requests.ts
@@ -0,0 +1,42 @@
+import axios from "axios"
+import { mapLesson } from "../mapper/lesson.mapper"
+
+const BASE_URL = 'http://localhost:8000/lesson/'
+
+export const getLessons = () => {
+  const lessons = axios.get(BASE_URL)
+    .then(response => {
+      const data = response.data.map((data: any) => mapLesson(data))
+      return data
+    })
+    .catch(error => {
+      return error
+    })
+  
+  return lessons
+}
+
+export const getLesson = (id: number) => {
+  const lesson = axios.get(`${BASE_URL}${id}/`)
+    .then(response => {
+      return mapLesson(response.data)
+    })
+    .catch(error => {
+      return error;
+    })
+
+  return lesson;
+}
+
+export const deleteLesson = (id: number) => {
+  const lesson = axios.delete(`${BASE_URL}${id}/`)
+    .then(response => {
+      console.log(response)
+      return response
+    })
+    .catch(error => {
+      return error
+    })
+
+  return lesson
+}
