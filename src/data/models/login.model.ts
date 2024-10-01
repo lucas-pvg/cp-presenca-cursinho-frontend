6275f76d65069d7b8bb981d65a5efde9db5e792f
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-09-30 21:02:31 -0300

    chore: prettier

--- src/data/models/login.model.ts
+++ src/data/models/login.model.ts
@@ -1,12 +1,12 @@
 export interface LoginData {
-    email: string
-    password: string
+  email: string;
+  password: string;
 }
 
 export interface ForgotPasswordData {
-    email: string
+  email: string;
 }
 
 export interface RefreshTokenData {
-    refresh: string
+  refresh: string;
 }
