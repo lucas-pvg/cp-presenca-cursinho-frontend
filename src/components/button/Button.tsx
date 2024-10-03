c190cec7c5941af412130a45a6fdfce3afb2d72b
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-02 21:57:49 -0300

    💄🚀 Added Present and Absent buttons as variants

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,40 +1,48 @@
-import { ComponentProps } from 'react';
-import { cva, VariantProps } from 'class-variance-authority';
-import { Link, To } from 'react-router-dom';
-import './Button.css';
+import { ComponentProps } from 'react'
+import { cva, VariantProps } from 'class-variance-authority'
+import { Link, To } from 'react-router-dom'
+import './Button.css'
 
-const ButtonVariants = cva('base-button', {
-  variants: {
-    variant: {
-      solid: 'solid',
-      outline: 'outline',
+
+const ButtonVariants = cva(
+  'base-button',
+  {
+    variants: {
+      variant: {
+        solid: 'solid',
+        outline: 'outline',
+        present: 'presence-button',
+        absent: 'abscense-button'
+      },
+      mode: {
+        dark: 'dark',
+        light: 'light'
+      }
     },
-    mode: {
-      dark: 'dark',
-      light: 'light',
-    },
-  },
-  defaultVariants: {
-    variant: 'solid',
-    mode: 'light',
-  },
-});
+    defaultVariants: {
+      variant: 'solid',
+      mode: 'light'
+    }
+  }
+)
 
 interface ButtonProps
-  extends ComponentProps<'button'>,
-    VariantProps<typeof ButtonVariants> {
-  to?: To;
-  variant?: 'solid' | 'outline';
-  mode?: 'light' | 'dark';
+  extends ComponentProps<'button'>, VariantProps<typeof ButtonVariants> {
+    to?: To
+    variant?: 'solid' | 'outline' | 'present' | 'absent'
+    mode?: 'light' | 'dark'
 }
 
 export function Button({ to, variant, mode, ...props }: ButtonProps) {
   if (to) {
     return (
       <Link to={to}>
-        <button className={ButtonVariants({ variant, mode })} {...props} />
+        <button className={ButtonVariants({variant, mode})} {...props} />
       </Link>
-    );
-  } else
-    return <button className={ButtonVariants({ variant, mode })} {...props} />;
+    )
+  }
+  
+  else return (    
+    <button className={ButtonVariants({variant, mode})} {...props} />
+  )
 }
