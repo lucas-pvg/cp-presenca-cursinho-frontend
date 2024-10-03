9099f8a8edeb3cd5f0276380f26efae3651f09c6
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-02 21:35:38 -0300

    🚀 Added text to all input and added class to SelectList to prevent css errors

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -9,22 +9,31 @@ import './input.css';
 
 interface inputProps extends ComponentProps<'input'> {
   mode?: 'light' | 'dark';
-  type: 'text' | 'date' | 'time' | 'select' | 'email' | 'password';
+  type: 'text' | 'date' | 'time' | 'email' | 'password';
   names?: Array<string>;
   values?: Array<string>;
+  label?: string;
 }
 
-export function Input({ mode, type, names, values, ...props }: inputProps) {
-  switch (type) {
-    case 'text':
-      return <TextInput mode={mode} {...props} />;
-    case 'date':
-      return <DateInput mode={mode} {...props} />;
-    case 'time':
-      return <TimeInput names={names} values={values} mode={mode} {...props} />;
-    case 'email':
-      return <EmailInput mode={mode} {...props} />;
-    case 'password':
-      return <PasswordInput mode={mode} {...props} />;
-  }
+export function Input({
+  mode,
+  type,
+  names,
+  values,
+  label,
+  ...props
+}: inputProps) {
+  return (
+    <div className="input-container">
+      {label && <h5>{label}</h5>}
+
+      {type == 'text' && <TextInput mode={mode} {...props} />}
+      {type == 'date' && <DateInput mode={mode} {...props} />}
+      {type == 'time' && (
+        <TimeInput names={names} values={values} mode={mode} {...props} />
+      )}
+      {type == 'email' && <EmailInput mode={mode} {...props} />}
+      {type == 'password' && <PasswordInput mode={mode} {...props} />}
+    </div>
+  );
 }
