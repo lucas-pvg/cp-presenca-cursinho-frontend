aeed64232cb9c0cef29f835b6c01d439e32db00b
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-02 22:24:06 -0300

    🚧🚀 partial deploy of student model, mapper and service

--- src/data/mapper/index.ts
+++ src/data/mapper/index.ts
@@ -1,5 +1,12 @@
 import { lessonMapper, lessonRequestMapper } from './lesson.mapper';
 import { subjectMapper } from './subject.mapper';
+import { studentMapper } from './student.mapper';
 import { studentClassMapper } from './student-class.mapper';
 
-export { lessonMapper, lessonRequestMapper, subjectMapper, studentClassMapper };
+export { 
+  lessonMapper,
+  lessonRequestMapper,
+  subjectMapper,
+  studentMapper,
+  studentClassMapper
+};
