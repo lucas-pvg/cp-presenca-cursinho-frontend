73388960a9f44f6935025d952c3b341bab538e24
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-07 03:55:58 -0300

    ✨🚀 Added NextClass card component

--- src/components/next-class/next-lesson.tsx
+++ src/components/next-class/next-lesson.tsx
@@ -1,44 +1,46 @@
-import { ComponentProps } from 'react'
-import { cva, VariantProps } from 'class-variance-authority'
-import { useNavigate } from 'react-router-dom'
-import { Lesson } from '../../data/models/lesson.model'
-import './next-lesson.css'
+import { ComponentProps } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+import { useNavigate } from 'react-router-dom';
+import { Lesson } from '../../data/models/lesson.model';
+import './next-lesson.css';
 
-const nextLessonVariants = cva(
-  'next-lesson',
-  {
-    variants: {
-      mode: {
-        light: 'light',
-        dark: 'dark'
-      }
+const nextLessonVariants = cva('next-lesson', {
+  variants: {
+    mode: {
+      light: 'light',
+      dark: 'dark',
     },
-    defaultVariants: {
-      mode: 'light'
-    }
-  }
-)
+  },
+  defaultVariants: {
+    mode: 'light',
+  },
+});
 
 interface nextLessonProps
-  extends ComponentProps<'div'>, VariantProps<typeof nextLessonVariants> {
-    mode?: 'light' | 'dark'
-    lesson: Lesson
+  extends ComponentProps<'div'>,
+    VariantProps<typeof nextLessonVariants> {
+  mode?: 'light' | 'dark';
+  lesson: Lesson;
 }
 
 export function NextLesson({ mode, lesson, ...props }: nextLessonProps) {
   const nav = useNavigate();
 
   return (
-    <div className={nextLessonVariants({ mode })} onClick={() => nav(`/lessons/${lesson.id}`)} {...props}>
-      <h6>{ lesson.subject }</h6>
+    <div
+      className={nextLessonVariants({ mode })}
+      onClick={() => nav(`/lessons/${lesson.id}`)}
+      {...props}
+    >
+      <h5>{lesson.subject}</h5>
 
-      <div className='next-lesson-info'>
-        <p>{ lesson.startTimeFormat() }</p>
+      <div className="next-lesson-info">
+        <p>{lesson.startTimeFormat()}</p>
 
-        <div className='next-lesson-class'>
-          <p>{ lesson.studentClass }</p>
+        <div className="next-lesson-class">
+          <p>{lesson.studentClass}</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
