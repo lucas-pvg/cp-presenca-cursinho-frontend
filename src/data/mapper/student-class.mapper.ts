b5afb6b42b761706d368a4a04d845f48b77910f6
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-07 15:59:31 -0300

    ✨🚀 Added StudentClass models, mapper and services

--- src/data/mapper/student-class.mapper.ts
+++ src/data/mapper/student-class.mapper.ts
@@ -3,12 +3,4 @@ import { StudentClassResponse } from '../models/student-class.model';
 
 export const studentClassMapper = (
   studentClass: StudentClassResponse
-): StudentClass => {
-  return {
-    id: studentClass.id,
-    name: studentClass.name,
-    classroom: studentClass.classroom,
-    course: studentClass.course,
-    subjects: studentClass.subjects,
-  };
-};
+): StudentClass => new StudentClass(studentClass);
--- src/data/models/student-class.model.ts
+++ src/data/models/student-class.model.ts
@@ -1,8 +1,9 @@
-export interface StudentClass {
+export interface StudentClassInterface {
   id: number;
   name: string;
   classroom: string;
   course?: string;
+  modality: string;
   subjects: number[];
 }
 
@@ -11,5 +12,44 @@ export interface StudentClassResponse {
   name: string;
   classroom: string;
   course?: string;
+  modality: string;
   subjects: number[];
+}
+
+export interface StudentClassRequest {
+  id?: number;
+  name: string;
+  classroom: string;
+  course?: string;
+  modality: string;
+  subjects: number[];
+}
+
+export class StudentClass implements StudentClassInterface {
+  id: number;
+  name: string;
+  classroom: string;
+  course?: string;
+  modality: string;
+  subjects: number[];
+
+  constructor(params: StudentClassInterface) {
+    this.id = params.id
+    this.name = params.name
+    this.classroom = params.classroom
+    this.course = params.course
+    this.modality = params.modality
+    this.subjects = params.subjects
+  }
+
+  modalityFormat(): string {
+    switch (this.modality) {
+      case 'ON':
+        return 'Online';
+      case 'IN':
+        return 'Presencial';
+      default:
+        return 'Modalidade não listada'
+    }
+  }
 }
