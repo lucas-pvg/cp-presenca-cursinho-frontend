c69175f5cc5af9afc5eac46a98fa6d0538cad104
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-09 15:49:36 -0300

    ✨ Update paths on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import 'react-toastify/dist/ReactToastify.css';
+import LoggedInLayout from './layouts/logged-in/LoggedInLayout';
+import LoggedOutLayout from './layouts/logged-out/LoggedOutLayout';
 
 import { HomePage } from './pages/home-page/HomePage';
 import { LessonsPage } from './pages/lessons-page/lessons-page';
@@ -8,13 +8,14 @@ import { LessonDetailPage } from './pages/lesson-detail-page/lesson-detail-page'
 import { SubjectPage } from './pages/subject-page/subject-page';
 import { SubjectDetailPage } from './pages/subject-detail-page/subject-detail-page';
 import { StudentClassPage } from './pages/student-class-page/StudentClassPage';
-import LoggedInLayout from './layouts/logged-in/LoggedInLayout';
-import LoggedOutLayout from './layouts/logged-out/LoggedOutLayout';
 import { ForgotPasswordPage } from './pages/forgot-password-page/ForgotPasswordPage';
 import { LoginPage } from './pages/login-page/LoginPage';
 import { useState } from 'react';
 import { UsersPage } from './pages/users-page/users-page';
 import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -25,25 +26,28 @@ function App() {
       <Routes>
         {isLoggedIn ? (
           <Route path="/" element={<LoggedInLayout />}>
-            <Route path="" element={<HomePage />} />
-            <Route path="lessons" element={<LessonsPage />} />
-            <Route path="lessons/:lessonID" element={<LessonDetailPage />} />
-            <Route path="subject" element={<SubjectPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/lessons" element={<LessonsPage />} />
+            <Route path="/lessons/:lessonID" element={<LessonDetailPage />} />
+            <Route path="/subject" element={<SubjectPage />} />
             <Route
-              path="subject/:subjectCode"
+              path="/subject/:subjectCode"
               element={<SubjectDetailPage />}
             />
+            <Route path="/classes" element={<StudentClassPage />} />
 
             <Route
-              path="attendance"
-              element={<h1 className="center">Presença</h1>}
+              path="/students"
+              element={<h1 className="center">Alunos</h1>}
             />
-            <Route path="users" element={<UsersPage />} />
             <Route
-              path="metrics"
+              path="/users"
+              element={<h1 className="center">Usuários</h1>}
+            />
+            <Route
+              path="/metrics"
               element={<h1 className="center">Métricas</h1>}
             />
-            <Route path="classes" element={<StudentClassPage />} />
           </Route>
         ) : (
           <Route path="/" element={<LoggedOutLayout />}>
