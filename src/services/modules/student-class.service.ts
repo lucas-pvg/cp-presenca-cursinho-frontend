828c3ca26195d60a9b3e2d57bc5623191a5694b7
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-09 14:24:42 -0300

    🚀✨ Add update and delete StudentClass services

--- src/services/modules/student-class.service.ts
+++ src/services/modules/student-class.service.ts
@@ -1,4 +1,4 @@
-import { get, post } from '../axios';
+import { get, post, patch, destroy } from '../axios';
 import { studentClassMapper } from '../../data/mapper';
 import { StudentClassRequest, StudentClassResponse } from '../../data/models/student-class.model';
 
@@ -13,13 +13,22 @@ const StudentClassService = {
   },
 
   async retrieveStudentClass(studentClassId: number) {
-    const response = await get(`subject/${studentClassId}/`);
+    const response = await get(`student_class/${studentClassId}/`);
     return studentClassMapper(response);
   },
 
   async createStudentClass(studentClass: StudentClassRequest, params?: unknown) {
     return await post('student_class/', studentClass, params);
   },
+
+  async updateStudentClass(studentClassId: number, studentClass: StudentClassRequest, params?: unknown) {
+    return await patch(`student_class/${studentClassId}/`, studentClass, params);
+  },
+
+  async deleteStudentClass(studentClassId: number) {
+    return await destroy(`student_class/${studentClassId}/`);
+  }
+
 };
 
 export default StudentClassService;
