ddf88272a5730a278591899cbe114491869414e2
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-09 15:49:36 -0300

    ✨ Update paths on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import 'react-toastify/dist/ReactToastify.css';
+import LoggedInLayout from './layouts/logged-in/LoggedInLayout';
+import LoggedOutLayout from './layouts/logged-out/LoggedOutLayout';
 
 import { HomePage } from './pages/home-page/HomePage';
 import { LessonsPage } from './pages/lessons-page/lessons-page';
 import { LessonDetailPage } from './pages/lesson-detail-page/lesson-detail-page';
 import { SubjectPage } from './pages/subject-page/subject-page';
 import { SubjectDetailPage } from './pages/subject-detail-page/subject-detail-page';
-import LoggedInLayout from './layouts/logged-in/LoggedInLayout';
-import LoggedOutLayout from './layouts/logged-out/LoggedOutLayout';
 import { ForgotPasswordPage } from './pages/forgot-password-page/ForgotPasswordPage';
 import { LoginPage } from './pages/login-page/LoginPage';
 import { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { StudentClassPage } from './pages/student-class-page/student-class-page';
+
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -22,42 +23,47 @@ function App() {
     <>
       <ToastContainer />
       <Routes>
-        {isLoggedIn ? (
-          <Route path="/" element={<LoggedInLayout />}>
-            <Route path="" element={<HomePage />} />
-            <Route path="lessons" element={<LessonsPage />} />
-            <Route path="lessons/:lessonID" element={<LessonDetailPage />} />
-            <Route path="subject" element={<SubjectPage />} />
-            <Route
-              path="subject/:subjectCode"
-              element={<SubjectDetailPage />}
-            />
+        {
+          isLoggedIn 
+          ? (
+            <Route path="/" element={<LoggedInLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/lessons" element={<LessonsPage />} />
+              <Route path="/lessons/:lessonID" element={<LessonDetailPage />} />
+              <Route path="/subject" element={<SubjectPage />} />
+              <Route path="/subject/:subjectCode" element={<SubjectDetailPage />} />
+              <Route path="/classes" element={<StudentClassPage />} />
 
-            <Route
-              path="attendence"
-              element={<h1 className="center">Presença</h1>}
-            />
-            <Route
-              path="metrics"
-              element={<h1 className="center">Métricas</h1>}
-            />
-            <Route path="classes" element={<StudentClassPage />} />
-          </Route>
-        ) : (
-          <Route path="/" element={<LoggedOutLayout />}>
-            <Route path="forgot-password" element={<ForgotPasswordPage />} />
-            <Route
-              path=""
-              element={
-                <LoginPage
-                  onLogin={() => {
-                    setIsLoggedIn(true);
-                  }}
-                />
-              }
-            />
-          </Route>
-        )}
+              <Route
+                path="/students"
+                element={<h1 className="center">Alunos</h1>}
+              />
+              <Route
+                path="/teachers"
+                element={<h1 className="center">Professores</h1>}
+              />
+              <Route
+                path="/metrics"
+                element={<h1 className="center">Métricas</h1>}
+              />
+            </Route>
+          ) 
+          : (
+            <Route path="/" element={<LoggedOutLayout />}>
+              <Route path="forgot-password" element={<ForgotPasswordPage />} />
+              <Route
+                path=""
+                element={
+                  <LoginPage
+                    onLogin={() => {
+                      setIsLoggedIn(true);
+                    }}
+                  />
+                }
+              />
+            </Route>
+          )
+        }
       </Routes>
     </>
   );
--- src/layouts/logged-in/LoggedInLayout.tsx
+++ src/layouts/logged-in/LoggedInLayout.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
-import './LoggedInLayout.css';
 import { Navbar } from '../../components/navbar/Navbar';
 import { Header } from '../../components/header/header';
+import './LoggedInLayout.css';
 
 const LoggedInLayout: React.FC = () => {
   const location = useLocation();
